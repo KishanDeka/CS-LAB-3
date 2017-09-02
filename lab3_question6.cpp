6372aed61f529ed722d2adeaabb778e2973bcbc2
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-09-02 20:14:45 +0530

    Update lab3_question6.cpp

--- lab3_question6.cpp
+++ lab3_question6.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <cmath>
 
@@ -8,7 +7,8 @@ int main ()
 { int a;
   cout << "enter the year you want to check"<<endl;
   cin >> a;
-  if (a%4==0) {cout <<"YES, it is a leap year.";}
-  else {cout << "NO, this is not a leap year.";}
+  if (a%100==0 && !(a%400==0)) {cout <<"It is not a leap year.";}
+  else if (a%4==0) {cout << "YES, it is a leap year.";}
+  else   {cout << "NO, this is not a leap year.";}
   return 0;
 }
