c7f1b0194316d4653d1e7b9473be16de7f4b4304
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-09-02 12:31:00 +0530

    Create lab3_question3.cpp

---
+++ lab3_question3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main () {
+float a;
+cout << "enter your number";
+cin >>a;
+if (a>0) {cout << "your number is positive.";}
+else if (a<0) {cout << "your number is negative.";}
+else { cout << "you have entered zero.";}
+return 0;
+}
