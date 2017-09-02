28c1abe0d63985faf43bfdbc460642b1ac7e17fd
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-09-02 15:22:46 +0530

    Create lab3_question5.cpp

---
+++ lab3_question5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main ()
+{ int a;
+  cout << "enter a integer number."<<endl;
+  cin >> a;
+  if (a%2==0) {cout <<"your number is even.";}
+  else {cout << "your number is odd.";}
+  return 0;
+}
