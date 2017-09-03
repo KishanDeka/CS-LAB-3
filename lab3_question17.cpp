935700f0126aa4843718afa7005fb702cb0783ea
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-09-03 14:14:15 +0530

    Update lab3_question17.cpp

--- lab3_question17.cpp
+++ lab3_question17.cpp
@@ -7,7 +7,9 @@ int main ()
 { float a, b, c;
   cout << "enter the value of a,b,c for quadratic equation of the form a(x)^2+bx+c=0 " <<endl;
   cin >>a>>b>>c;
-  if (pow(b,2)>=4*a*c) {cout << "root 1= "<<(-b+sqrt(pow(b,2)-(4*a*c)))/(2*a) <<endl<<"root 2= " <<(-b-sqrt(pow(b,2)-(4*a*c)))/(2*a);}
+  if (a==0 || (a==0 && b==0)) {cout << "The equation is not quadratic.";}
+  else if (a==0 && b==0 && c==0) {cout << "The equation has infinite roots";}
+  else if (pow(b,2)>=4*a*c) {cout << "root 1= "<<(-b+sqrt(pow(b,2)-(4*a*c)))/(2*a) <<endl<<"root 2= " <<(-b-sqrt(pow(b,2)-(4*a*c)))/(2*a);}
   else {cout << "root 1= " <<((-b)/(2*a))<<"+"<<"i("<<sqrt(-(pow(b,2)-(4*a*c)))/(2*a)<<")"<<endl<<"root 2= "<<((-b)/(2*a))<<"-"<<"i("<<sqrt(-(pow(b,2)-(4*a*c)))/(2*a)<<")" ;}
   return 0;
 }
