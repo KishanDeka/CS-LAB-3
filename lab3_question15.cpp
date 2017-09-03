825d97e92f8fdb6c2bdc318ce8cf3cb195ef53a4
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-09-03 12:22:35 +0530

    Create lab3_question15.cpp

---
+++ lab3_question15.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{ float a, b, c;
+  cout << "enter the three sides of a triangle " <<endl;
+  cin >>a>>b>>c;
+  if (a+b>c && a+c>b && b+c>a) {cout << "the triangle is valid.";}
+  else {cout << "the triangle is not valid.";}
+  return 0;
+}
