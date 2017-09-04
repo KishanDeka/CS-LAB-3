fb35d9a31d026eb8a2e87cf377c6539439e1d384
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-09-04 10:05:54 +0530

    Create lab3_question25.cpp

---
+++ lab3_question25.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{ int a=2;
+cout << "the even numbers between 1 and 100 are listed below "<<endl;
+  while (a<=100) {cout << a<<endl;
+                     a=a+2;
+                      }
+
+
+  return 0;
+}
