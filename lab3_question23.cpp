791beea96333b84b16da0338d7811fb25ffbe6b2
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-09-04 09:57:38 +0530

    Create lab3_question23.cpp

---
+++ lab3_question23.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{ int a, n;
+  cout << "Enter any natural number to write all natural number to that number ";
+  cin >> n;
+  a=n;
+  while (a>=1) {cout << a<<endl;
+                     a--;
+                      }
+
+
+  return 0;
+}
