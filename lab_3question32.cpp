82b5932214cd64f678315fd75476abbdc56cde01
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-09-05 19:13:56 +0530

    Update lab_3question32.cpp

--- lab_3question32.cpp
+++ lab_3question32.cpp
@@ -4,17 +4,20 @@ using namespace std;
 
 int main ()
 { int a, n, m, h;
-  cout << "enter the number ";
+  cout << "enter the number "<<endl;
   cin >> a;
+
+  if (a/10==0){if (a>=0) {cout << "first digit of the number is "<<a<<endl;}
+  else {cout << "first digit of the number is "<<-a<<endl;} }
+  else if (a<0){h=a/10;
+  while (h<=-10) { h=h/10;}
+  cout <<"first digit of the number is "<<-h<<endl;}
+  else {h=a/10;
+  while (h>=10) { h=h/10 ;}
+  cout << "first digit of the number is "<<h<<endl;}
   n=a/10;
   m=n*10;
-  while (h>=10) { h=a/10;
-                 h=h/10;
-
-}
-cout << "first digit of the number is "<<h<<endl;
-  cout << "last digit of the number is "<<a-m;
-
-
+  if (a>=0) {cout << "last digit of the number is "<<a-m;}
+  else {cout <<"last digit of the number is "<<-a+m;}
   return 0;
  }
