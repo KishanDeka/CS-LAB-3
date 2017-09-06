176d28ed95c537a5738cad3d2c1cf4658315836a
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-09-06 23:45:26 +0530

    Create lab_question34.cpp

---
+++ lab_question34.cpp
@@ -0,0 +1,18 @@
+int main ()
+{ int a, n, m, h, c=0, k, i, j, l, p, q;
+  cout << "enter a integer number "<<endl;
+  cin >> a;
+  m=abs(a);
+  i=m;
+  n=a%10;
+ while (i>=10) {i=i/10 ;}
+ j=m;
+ while (j>=1) { j=j/10;
+                c++; }
+ k=(i*pow(10,c-1))+1;
+ l=m-n-k;
+ p=(n*pow(10,c-1))+1;
+ q=(p+l)+i;
+ cout << "After swapping the first and last digit = "<< q;
+  return 0;
+ }
